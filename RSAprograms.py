f5f2e81483544aac8a77f8dc75f9506d166087c1
Farida-elgarhy <fe2200329@tkh.edu.eg>
2024-04-18 15:47:59 +0200

    Update RSAprograms.py

--- RSAprograms.py
+++ RSAprograms.py
@@ -24,8 +24,19 @@ def generate_random_prime_number(bit_length = 8):
        number = random.getrandbits(bit_length)
        if number % 2 != 0 and prime_numbers(number) and number.bit_length() == bit_length:
            return number
-       
+
+#generating random primes for p and q       
 p=generate_random_prime_number(8)
 q=generate_random_prime_number(8)
 
+#ensuring that p and q are not the same number
+while p == q:
+    q = generate_random_prime_number(8)
+
+#calculating n and totient
+n=p*q
+eul=(p-1)*(q-1)
+e=3
+
 print(f"p is: {p} and q is: {q}")
+print("n is: ", n, "\neul is: ", eul, "\ne is: ", e)
