39e10c2728e101afd60cf1c85a66b71551d9bcca
Farida-elgarhy <fe2200329@tkh.edu.eg>
2024-04-20 17:11:37 +0200

    Update keygeneration.py

--- keygeneration.py
+++ keygeneration.py
@@ -1,3 +1,17 @@
 import math
 import random
 import time 
+
+#to make sure number is prime 
+def prime_numbers(n):
+   if n<=1:
+       return False
+   if n==2:
+       return True
+   if n % 2 == 0 and n > 2:
+       return False
+   else:
+       for i in range(3, int(math.sqrt(n) + 1), 2):
+           if n % i == 0:
+               return False
+       return True
